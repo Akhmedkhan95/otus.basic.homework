bdef2de45c0239e05f3c99671dbbb700ddfd0833
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-03-09 13:47:06 +0300

    Изменения в исключении DataException

--- src/main/java/ru/otus/hw8/AppArrayDataException.java
+++ src/main/java/ru/otus/hw8/AppArrayDataException.java
@@ -1,7 +1,20 @@
 package ru.otus.hw8;
 
 public class AppArrayDataException extends RuntimeException {
+    private final int row;
+    private final int col;
+
     public AppArrayDataException ( int row, int col) {
-        System.out.println(("В строке " + row + " столбце " + col + " произошла ошибка"));
+        super("Ошибка в ячейке [" + row + "][" + col + "]");
+        this.col = col;
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
     }
 }
