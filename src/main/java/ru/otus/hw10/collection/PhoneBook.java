0ff046e822e50acc201dd89e94ec855de5b98880
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-03-17 09:17:05 +0300

    Finished HW 10.1

--- src/main/java/ru/otus/hw10/collection/PhoneBook.java
+++ src/main/java/ru/otus/hw10/collection/PhoneBook.java
@@ -6,33 +6,34 @@ import java.util.List;
 import java.util.Map;
 
 public class PhoneBook {
-    private static Map<String, List<String>> guid;
+    private  Map<String, List<String>> guid;
 
     public PhoneBook() {
         guid = new HashMap<>();
     }
 
-    public static void add (String name, String phoneNumber) {
+    public void add(String name, String phoneNumber) {
         if (guid.containsKey(name)) {
             List<String> phoneNumbers = guid.get(phoneNumber);
             phoneNumbers.add(phoneNumber);
         } else {
             List<String> phoneNumbers = new ArrayList<>();
             phoneNumbers.add(phoneNumber);
-            guid.put(name,phoneNumbers);
+            guid.put(name, phoneNumbers);
         }
     }
 
-    public static void find (String name) {
+    public String find(String name) {
         guid.get(name);
+        return name;
     }
 
-    public static boolean containsPhoneNumber (String phoneNumber) {
-        for(Map.Entry<String, List<String>> entry : guid.entrySet()) {
+    public boolean containsPhoneNumber(String phoneNumber) {
+        for (Map.Entry<String, List<String>> entry : guid.entrySet()) {
             List<String> phoneNumbers = entry.getValue();
-             if (phoneNumbers.contains(phoneNumber)) {
-                 return true;
-             }
+            if (phoneNumbers.contains(phoneNumber)) {
+                return true;
+            }
         }
         return false;
     }
