9c73757c7b5798a1fc05338f26f6b2a1e05da396
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-03-19 21:31:36 +0300

    Finished HW12

---
+++ src/main/java/ru/otus/hw12/UtilScanner.java
@@ -0,0 +1,11 @@
+package ru.otus.hw12;
+
+import java.util.Scanner;
+
+public class UtilScanner {
+
+    public static String executeScanner() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
