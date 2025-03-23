335eb84ccd3e9d3176a197aa1c568c3827201365
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-03-23 18:01:34 +0300

    Init HW13

---
+++ src/main/java/ru/otus/hw13/HomeWork13.java
@@ -0,0 +1,7 @@
+package ru.otus.hw13;
+
+public class HomeWork13 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/otus/hw13/Server.java
@@ -0,0 +1,13 @@
+package ru.otus.hw13;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+
+}
