8aa2e675cb3013ac0abf8cd8fa3081d28332eff9
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-03-23 23:14:44 +0300

    Finished HW 13

--- src/main/java/ru/otus/hw13/HomeWork13.java
+++ src/main/java/ru/otus/hw13/HomeWork13.java
@@ -1,7 +1,29 @@
 package ru.otus.hw13;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class HomeWork13 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try (Socket socket = new Socket("localhost", 1010)) {
+                Client client = new Client(socket.getInputStream(), socket.getOutputStream());
+                String dateFromServer = client.get();
+                System.out.println("Введите два числа и выберите операцию: (2 4 +) " + dateFromServer);
+                String dateToSend = scanner.nextLine();
+                if (dateToSend.equalsIgnoreCase("exit")) {
+                    client.send(dateToSend);
+                    break;
+                }
+                client.send(dateToSend);
+                dateFromServer = client.get();
+                System.out.println(dateFromServer);
+            } catch (IOException e) {
+                System.out.println("client");
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/main/java/ru/otus/hw13/Server.java
+++ src/main/java/ru/otus/hw13/Server.java
@@ -9,5 +9,34 @@ import java.net.Socket;
 
 public class Server {
 
+    public static void main(String[] args) throws IOException {
+        Calculator calculator = new Calculator();
+        ServerSocket socket = new ServerSocket(1010);
+        System.out.println("Сервер запущен!");
+        while (true) {
+            Socket client = socket.accept();
+            DataInputStream inputStream = new DataInputStream(client.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
+            String operations = calculator.getListOperation();
+            outputStream.writeUTF(operations);
+            String userInput = inputStream.readUTF();
+            System.out.println("Пользователь ввел данные: " + userInput);
+            if (userInput.equalsIgnoreCase("exit")) {
+                System.out.println("Клиент отключился");
+                client.close();
+                continue;
+            }
+            String[] dates = parsDateFromClient(userInput);
+            double numOne = Double.parseDouble(dates[0]);
+            double numTwo = Double.parseDouble(dates[1]);
+            String operation = dates[2];
+            double res = calculator.calc(numOne, numTwo, operation);
+            outputStream.writeUTF(String.valueOf(res));
+        }
+    }
+
+    public static String[] parsDateFromClient(String date) {
+        return date.split(" ");
+    }
 
 }
