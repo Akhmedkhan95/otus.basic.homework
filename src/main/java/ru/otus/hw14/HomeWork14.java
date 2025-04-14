0d862238c09f1fdb2e540e0142bad32f6cd3191c
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-04-14 10:27:47 +0300

    Finish HW 14

--- src/main/java/ru/otus/hw14/HomeWork14.java
+++ src/main/java/ru/otus/hw14/HomeWork14.java
@@ -1,7 +1,41 @@
 package ru.otus.hw14;
 
 public class HomeWork14 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        double[] array = new double[100_000_000];
+        long before1 = System.currentTimeMillis();
+        System.out.println(Thread.currentThread());
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+        }
+        long after1 = System.currentTimeMillis();
+        System.out.println((after1 - before1) / 1000);
+
+        long before2 = System.currentTimeMillis();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 25_000_000; i++) {
+                array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 25_000_001; i < 50_000_000; i++) {
+                array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+            }
+        });
+        Thread thread3 = new Thread(() -> {
+            for (int i = 50_000_001; i < 75_000_000; i++) {
+                array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+            }
+        });
+        Thread thread4 = new Thread(() -> {
+            for (int i = 75_000_001; i < 100_000_000; i++) {
+                array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+            }
+        });
+
+        long after2 = System.currentTimeMillis();
+        System.out.println(after2 - before2 / 1000);
 
     }
 }
