987743578b6d24437079f90bf46a5f18ca4f03ee
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-04-14 12:50:44 +0300

    Finish HW 14 (исправления в сравнении)

--- src/main/java/ru/otus/hw14/HomeWork14.java
+++ src/main/java/ru/otus/hw14/HomeWork14.java
@@ -18,17 +18,17 @@ public class HomeWork14 {
             }
         });
         Thread thread2 = new Thread(() -> {
-            for (int i = 25_000_001; i < 50_000_000; i++) {
+            for (int i = 25_000_000; i < 50_000_000; i++) {
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
         Thread thread3 = new Thread(() -> {
-            for (int i = 50_000_001; i < 75_000_000; i++) {
+            for (int i = 50_000_000; i < 75_000_000; i++) {
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
         Thread thread4 = new Thread(() -> {
-            for (int i = 75_000_001; i < 100_000_000; i++) {
+            for (int i = 75_000_000; i <= 100_000_000; i++) {
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
